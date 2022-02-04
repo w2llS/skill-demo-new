e0d254325efdd60a3591bb655f42281624b28eed
w2lls <1car2wills@gmail.com>
2022-02-04 01:48:19 -0700

    added skilldemotest (fail)

---
+++ SkillDemoTest.java
@@ -0,0 +1,13 @@
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SkillDemoTest
+{
+    @Test 
+    public void Division()
+    {
+        assertEquals(2, SkillDemo.Division(6, 2)); // this should fail
+    }
+ 
+}
